974d7526e65241acc60e26c93d6489708e96e84d
Kirill <kirill551@yandex.ru>
2025-06-28 11:01:58 +0200

    "Добавить конфигурацию с настройкой маршрутизации Ingress на эндпоинт goals"

--- load-kv.sh
+++ load-kv.sh
@@ -1,14 +1,13 @@
 
-echo "⏳ Ожидаем запуска Consul..."
+echo "⏳ Starting Consul..."
 until curl -s http://localhost:8500/v1/status/leader | grep -q '"'; do
   sleep 1
 done
-echo "✅ Consul доступен. Загружаем KV-данные..."
+echo "✅ Loading values..."
 
-for key in $(jq -r 'keys[]' /kv-data.json); do
-  value=$(jq -r --arg key "$key" '.[$key]' /kv-data.json)
-  echo "📥 Загружаем $key = $value"
+for key in $(jq -r 'keys[]' /kv-data.json); do value=$(jq -r --arg key "$key" '.[$key]' /kv-data.json)
+  echo "📥 Loading $key = $value"
   curl -s -X PUT --data "$value" http://localhost:8500/v1/kv/$key > /dev/null
 done
 
-echo "✅ Загрузка завершена"
+echo "✅ Loading finished"
